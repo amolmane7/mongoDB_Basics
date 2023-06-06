097d5d9794624aca6a412f303b4731af1e72c6c7
Amol_Mane <amol_mane@Amol-MacBook-Pro.local>
2023-06-06 14:32:10 +0530

    [Amol] : added mongoDB pgm for create collections and drop collections

--- mongoDB.js
+++ mongoDB.js
@@ -17,3 +17,22 @@ show dbs
 //drop database
 
 db.dropDatabase()
+
+//create collection and drop collection
+
+db.createCollection("myCollection")
+
+db.myCollection2.insertOne({
+                            "firstName" : "Amit" ,
+                            "lastName"  : "Singh" 
+})
+
+//show all collections
+
+show collections
+
+//delete collection
+
+db.myCollection2.drop()
+
+show collections
