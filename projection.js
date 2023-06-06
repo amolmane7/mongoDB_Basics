3f2267ad88435a80c1ab0d34334bddd9a9ada2a8
Amol_Mane <amol_mane@Amol-MacBook-Pro.local>
2023-06-06 17:13:04 +0530

    [Amol] : modified mongoDB pgm for projection

--- projection.js
+++ projection.js
@@ -6,3 +6,7 @@ db.students.find({},{"firstName" : 1 , "_id" : 0})
 
 //fetching ages of all students
 db.students.find({},{"age" : 1 , "_id" : 0})
+
+db.students.aggregate([{$project :{"lastName":1 , "_id": 0}}])
+
+db.students.find({},{"firstName" : 1,"lastName":1 , "_id" : 0})
