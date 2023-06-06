b8bf01cb9d4bdde5629730306d0c3af3b608b62f
Amol_Mane <amol_mane@Amol-MacBook-Pro.local>
2023-06-06 14:49:05 +0530

    [Amol] : added mongoDB pgm for insert documents

---
+++ insert.js
@@ -0,0 +1,21 @@
+use school
+
+db.students.insert([{
+                    "studentNo" : "1",
+                    "firstName" : "Ajay",
+                    "lastName"  : "Shelke",
+                    "age"       : "22"
+                    
+                    },{
+                    "studentNo" : "2",
+                    "firstName" : "Rohit",
+                    "lastName"  : "Kumar",
+                    "age"       : "30"
+                    },
+                    {
+                    "studentNo" : "3",
+                    "firstName" : "Amit",
+                    "lastName"  : "Singh",
+                    "age"       : "26"
+                    }])
+                    
