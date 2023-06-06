4cfa9ee93b0e54f64ae81bf97329c7bb7e494cab
Amol_Mane <amol_mane@Amol-MacBook-Pro.local>
2023-06-06 14:15:16 +0530

    [Amol] : added mongoDB basic programs

---
+++ mongoDB.js
@@ -0,0 +1,19 @@
+db
+
+show dbs
+
+//create database
+
+use myDb
+
+db
+
+db.mycollection.insertOne({
+                        "name" : "Aman"
+                        })
+                        
+show dbs
+
+//drop database
+
+db.dropDatabase()
